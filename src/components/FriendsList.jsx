1978cb8751138299e2e462704c9ea4a98f516cef
DeveloperVaibhav <vaibhavchafle09@gmail.com>
2026-01-28 22:21:35 +0530

    refactor code in separate components

---
+++ src/components/FriendsList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Friend } from "./Friend.jsx";
+
+export function FriendsList({ friends, onSelection, selectedFriend }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          friend={friend}
+          key={friend.id}
+          selectedFriend={selectedFriend}
+          onSelection={onSelection}
+        />
+      ))}
+    </ul>
+  );
+}
